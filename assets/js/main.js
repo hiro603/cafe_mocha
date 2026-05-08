21e5bffd6424ed2d5257c3d3b80865aa1086bcf7
nng88 <200077567+hiro603@users.noreply.github.com>
2026-05-08 16:19:51 +0900

    feat: implement menu section with Splide slider and responsive grid layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import { initializeHamburgerMenu } from "./component/hamburger-menu.js";
 import { initializeHeaderBackgroundToggle } from "./component/header-background-toggle.js";
-import { initializeCultureGallery } from "./slider/gallery-slider.js";
+import { initializeMenuSlider } from "./slider/menu-slider.js";
 import { initializeAboutStaffSlider } from "./slider/staff-slider.js";
 import { initializeAccordionMenu } from "./component/accordion-menu.js";
 // import { initializeFormValidation } from "./form/form-validation.js";
@@ -8,7 +8,7 @@ import { initializeAccordionMenu } from "./component/accordion-menu.js";
 // 全ての初期化を実行
 initializeHamburgerMenu();
 initializeHeaderBackgroundToggle();
-initializeCultureGallery();
+initializeMenuSlider();
 initializeAboutStaffSlider();
 initializeAccordionMenu();
 // initializeFormValidation();
